57f9dcae5559b0ec85f49d6671382b1cd0adad93
xeoron <aitsinformation@gmail.com>
2013-03-19 21:05:39 -0400

    added - general numeric sort

--- idownloaded_sh/idownloaded.sh
+++ idownloaded_sh/idownloaded.sh
@@ -11,15 +11,20 @@ do
     -h|--help)
         echo "Usage: idownloaded.sh <options>"
         echo "-l --list the files that have been downloaded"
+	echo "-lr --list-sort lists the files that have been downloaded in a general-numeric sorted order"
         echo "-d --delete the download file list"
         echo "-v --version"
         echo "-h --help"
         exit 0
         ;;
-	-l|--list)
+    -l|--list)
         echo "Downloaded file list:"
     	sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select LSQuarantineDataURLString from LSQuarantineEvent'
         ;;
+    -lr|--list-sort)
+	echo "Downloaded sorted file list:"
+        sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select LSQuarantineDataURLString from LSQuarantineEvent' | sort -g
+        ;; 
     -d|--delete)
         echo "Deleting system list of downloaded files..."
     	sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'
